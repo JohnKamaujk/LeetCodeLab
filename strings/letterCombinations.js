61f34dd75cbef2a80c666f1c798206b6a65d30be
JohnKamaujk <johnjkkamau@gmail.com>
2023-05-30 21:17:30 +0300

    sparkles:chore:remove occurence of an element

--- strings/letterCombinations.js
+++ strings/letterCombinations.js
@@ -1,3 +1,7 @@
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+// A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
 /**
  * @param {string} digits
  * @return {string[]}
@@ -35,5 +39,3 @@ var letterCombinations = function (digits) {
 
   return mapper(digits);
 };
-
-// console.log(letterCombinations("21"));
