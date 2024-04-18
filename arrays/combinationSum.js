7b3ad312c3c7e3c308879fb6d9035cc0d9afb4ce
JohnKamaujk <johnjkkamau@gmail.com>
2024-04-18 11:30:55 +0300

    :white_check_mark:combination sum solution

--- arrays/combinationSum.js
+++ arrays/combinationSum.js
@@ -3,7 +3,6 @@
 The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
 The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
 
-
 Example 1:
 Input: candidates = [2,3,6,7], target = 7
 Output: [[2,2,3],[7]]
@@ -19,6 +18,7 @@ Output: [[2,2,2,2],[2,3,3],[3,5]]
 Example 3:
 Input: candidates = [2], target = 1
 Output: []
+
 * @param {number[]} candidates
 * @param {number} target
 * @return {number[][]}
