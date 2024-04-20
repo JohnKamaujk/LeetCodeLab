bc07bd64e796be89175ad3f53e5c3bae55ccdf09
JohnKamaujk <johnjkkamau@gmail.com>
2024-04-20 18:11:14 +0300

    :white_check_mark:number islands solution

--- arrays/numIslands.js
+++ arrays/numIslands.js
@@ -24,7 +24,25 @@ Output: 3
 * @param {character[][]} grid
 * @return {number}
 */
-var numIslands = function(grid) {
-    
+var numIslands = function (grid) {
+  const rows = grid.length,
+    cols = grid[0].length;
+  let islands = 0;
+  const dfs = (r, c) => {
+    if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] !== "1") return;
+    grid[r][c] = 0; //mark land as checked
+    dfs(r - 1, c); // check top of cell
+    dfs(r + 1, c); // check bottom of cell
+    dfs(r, c + 1); // check right of cell
+    dfs(r, c - 1); // check left of cell
+  };
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (grid[r][c] == "1") {
+        dfs(r, c);
+        islands++;
+      }
+    }
+  }
+  return islands;
 };
-
