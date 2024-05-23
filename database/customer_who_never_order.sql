1e6fc9a9386bf07ba895a64169779d5dbdd34f1a
JohnKamaujk <johnjkkamau@gmail.com>
2024-05-23 22:10:56 +0300

    :white_check_mark:customer who never order solution

--- database/customer_who_never_order.sql
+++ database/customer_who_never_order.sql
@@ -52,3 +52,8 @@
 -- | Henry     |
 -- | Max       |
 -- +-----------+
+SELECT Customers.name AS Customers
+FROM Customers
+LEFT JOIN Orders
+ON Customers.id = Orders.customerId
+WHERE Orders.customerId is null;
