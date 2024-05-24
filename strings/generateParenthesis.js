bee609e3688091b20cd499e28428f8c5d2f37ac7
JohnKamaujk <johnjkkamau@gmail.com>
2024-05-24 20:37:09 +0300

    :white_check_mark:generate parenthesis solution

--- strings/generateParenthesis.js
+++ strings/generateParenthesis.js
@@ -11,4 +11,19 @@ Output: ["()"]
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function (n) {};
+var generateParenthesis = function (n) {
+  const stack = [];
+
+  const backtrack = (str, open, close) => {
+    if (open + close === n * 2) {
+      stack.push(str);
+    }
+
+    if (open < n) backtrack(str + "(", open + 1, close);
+    if (close < open) backtrack(str + ")", open, close + 1);
+  };
+
+  backtrack("", 0, 0);
+
+  return stack;
+};
