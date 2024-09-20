36852adf901629f020dc79468d4ffed3809e930f
JohnKamaujk <johnjkkamau@gmail.com>
2024-09-20 22:47:37 +0300

    :white_check_mark:chore:valid sudoku

--- arrays/validSudoku.js
+++ arrays/validSudoku.js
@@ -39,3 +39,33 @@ Explanation: Same as Example 1, except with the 5 in the top left corner being m
 * @param {character[][]} board
 * @return {boolean}
 */
+
+var isValidSudoku = function (board) {
+  const hashset = new Set();
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const subBox = Math.floor(i / 3) * 3 + Math.floor(j / 3); //formula to calculate 3X3 matrix
+      const subBoxString = `${subBox}-${board[i][j]}`; // to store unique value in 3x3 matrix
+      const rowString = `row-${i}-${board[i][j]}`; // to store unique value in row
+      const columnString = `column-${j}-${board[i][j]}`; // to store unique value in column
+
+      if (
+        hashset.has(subBoxString) ||
+        hashset.has(rowString) ||
+        hashset.has(columnString)
+      ) {
+        return false; //if already present in hashset then return false
+      }
+      if (board[i][j] === ".") {
+        // don't store "." in hashset
+        continue;
+      }
+      // store value in the set
+      hashset.add(subBoxString);
+      hashset.add(rowString);
+      hashset.add(columnString);
+    }
+  }
+  return true;
+};
