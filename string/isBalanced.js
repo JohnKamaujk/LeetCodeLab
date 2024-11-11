6eba4198de4a8c8199e4ab7e6599f7c999ad4551
JohnKamaujk <johnjkkamau@gmail.com>
2024-11-11 21:33:53 +0300

    :white_check_mark: is balanced

--- string/isBalanced.js
+++ string/isBalanced.js
@@ -22,6 +22,15 @@ Since both are equal the num is balanced.
 * @param {string} num
 * @return {boolean}
 */
-var isBalanced = function(num) {
-    
+var isBalanced = function (num) {
+  let evenSum = 0;
+  let oddSum = 0;
+  for (let i = 0; i < num.length; i++) {
+    if (i % 2 == 0) {
+      evenSum += parseInt(num[i]);
+    } else {
+      oddSum += parseInt(num[i]);
+    }
+  }
+  return evenSum == oddSum;
 };
