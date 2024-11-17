daa6b71c6a132176d985c0412df440892b7fe6f2
JohnKamaujk <johnjkkamau@gmail.com>
2024-11-17 22:21:53 +0300

    :sparkles:feat: most words found

--- array/mostWordsFound.js
+++ array/mostWordsFound.js
@@ -23,6 +23,11 @@ In this example, the second and third sentences (underlined) have the same numbe
 * @param {string[]} sentences
 * @return {number}
 */
-var mostWordsFound = function(sentences) {
-    
+var mostWordsFound = function (sentences) {
+  let count = 0;
+  for (let i = 0; i < sentences.length; i++) {
+    let wordsCount = sentences[i].split(" ").length;
+    count = Math.max(count, wordsCount);
+  }
+  return count;
 };
