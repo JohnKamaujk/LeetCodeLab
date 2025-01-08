ab73f0d7c0db7db7e2db58cda785ab3d4af859a6
JohnKamaujk <johnjkkamau@gmail.com>
2025-01-08 14:56:30 +0300

    :white_check_mark:merge K lists

---
+++ linked_list/mergeKLists .js
@@ -0,0 +1,55 @@
+/**
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+Example 1:
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+
+Example 2:
+Input: lists = []
+Output: []
+
+Example 3:
+Input: lists = [[]]
+Output: []
+
+* Definition for singly-linked list.
+* function ListNode(val, next) {
+*     this.val = (val===undefined ? 0 : val)
+*     this.next = (next===undefined ? null : next)
+* }
+
+* @param {ListNode[]} lists
+* @return {ListNode}
+*/
+var mergeKLists = function (lists) {
+  function mergeTwoLists(list1, list2) {
+    let dummy = new ListNode(0);
+    let current = dummy;
+
+    while (list1 && list2) {
+      if (list1.val < list2.val) {
+        current.next = list1;
+        list1 = list1.next;
+      } else {
+        current.next = list2;
+        list2 = list2.next;
+      }
+      current = current.next;
+    }
+
+    current.next = list1 || list2;
+
+    return dummy.next;
+  }
+};
