0624e2ea44777fa1c5a98fddd832fcac415c3de4
JohnKamaujk <johnjkkamau@gmail.com>
2025-01-26 16:15:13 +0300

    :white_check_mark:unique paths

--- math/uniquePaths.js
+++ math/uniquePaths.js
@@ -21,4 +21,21 @@ Explanation: From the top-left corner, there are a total of 3 ways to reach the 
 * @param {number} n
 * @return {number}
 */
-var uniquePaths = function (m, n) {};
+var uniquePaths = function (m, n) {
+  const dp = Array.from({ length: m }, () => Array(n).fill(0));
+
+  for (let i = 0; i < m; ++i) {
+    dp[i][0] = 1;
+  }
+  for (let j = 0; j < n; ++j) {
+    dp[0][j] = 1;
+  }
+
+  for (let i = 1; i < m; ++i) {
+    for (let j = 1; j < n; ++j) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+
+  return dp[m - 1][n - 1];
+};
