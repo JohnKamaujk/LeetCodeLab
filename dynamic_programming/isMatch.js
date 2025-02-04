925da240894f136f4f6f5df108eb43c10e2bb726
JohnKamaujk <johnjkkamau@gmail.com>
2025-02-04 12:57:42 +0300

    :white_check_mark:is match

--- dynamic_programming/isMatch.js
+++ dynamic_programming/isMatch.js
@@ -25,6 +25,28 @@ Explanation: ".*" means "zero or more (*) of any character (.)".
 * @param {string} p
 * @return {boolean}
 */
-var isMatch = function(s, p) {
-    
+var isMatch = function (s, p) {
+  if (!p) return !s;
+  const memo = new Map();
+
+  const dp = (i, j) => {
+    const key = `${i},${j}`;
+    if (memo.has(key)) return memo.get(key);
+
+    if (j === p.length) return i === s.length;
+
+    const firstMatch = i < s.length && (s[i] === p[j] || p[j] === ".");
+
+    let result;
+    if (j + 1 < p.length && p[j + 1] === "*") {
+      result = dp(i, j + 2) || (firstMatch && dp(i + 1, j));
+    } else {
+      result = firstMatch && dp(i + 1, j + 1);
+    }
+
+    memo.set(key, result);
+    return result;
+  };
+
+  return dp(0, 0);
 };
