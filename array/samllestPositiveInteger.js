1230b30ec80149aaee7345636273d2376db0e3dc
JohnKamaujk <johnjkkamau@gmail.com>
2025-02-28 12:54:43 +0300

    :white_check_mark:smallest missing positive integer

--- array/samllestPositiveInteger.js
+++ array/samllestPositiveInteger.js
@@ -18,3 +18,26 @@ Input: nums = [7,8,9,11,12]
 Output: 1
 Explanation: The smallest positive integer 1 is missing.
 */
+
+function missingNumber(arr) {
+  arr.sort((a, b) => a - b);
+
+  // ans will hold the current smallest missing number,
+  // initially set to 1
+  let res = 1;
+  for (let i = 0; i < arr.length; i++) {
+    // If we have found 'res' in the array,
+    // 'res' is no longer missing, so increment it
+    if (arr[i] == res) {
+      res++;
+    }
+
+    // If the current element is larger than 'res',
+    // 'res' cannot be found in the array,
+    // so it is our final answer
+    else if (arr[i] > res) {
+      break;
+    }
+  }
+  return res;
+}
