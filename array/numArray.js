01b9f9a32e5bafa91baa6de844015350a95a3aa7
JohnKamaujk <johnjkkamau@gmail.com>
2025-03-05 12:51:47 +0300

    :white_check_mark: Range sum query - immutable

--- array/numArray.js
+++ array/numArray.js
@@ -23,14 +23,21 @@ numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
 * @param {number[]} nums
 */
 
-var NumArray = function (nums) {};
+var NumArray = function (nums) {
+  this.sum = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    this.sum[i] = this.sum[i - 1] + nums[i];
+  }
+};
 
 /**
  * @param {number} left
  * @param {number} right
  * @return {number}
  */
-NumArray.prototype.sumRange = function (left, right) {};
+NumArray.prototype.sumRange = function (left, right) {
+  return this.sum[right] - (this.sum[left - 1] || 0);
+};
 
 /**
  * Your NumArray object will be instantiated and called as such:
