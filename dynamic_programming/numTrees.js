6cdac939c5000bcf83253fcf685c9180f3ff2167
JohnKamaujk <johnjkkamau@gmail.com>
2025-03-14 13:09:25 +0300

    :white_check_mark: Unique binary search trees

--- dynamic_programming/numTrees.js
+++ dynamic_programming/numTrees.js
@@ -12,5 +12,16 @@ Output: 1
 * @param {number} n
 * @return {number}
 */
-var numTrees = function (n) {};
+var numTrees = function (n) {
+  let uniqTree = Array(n + 1).fill(1);
 
+  for (let nodes = 2; nodes <= n; nodes++) {
+    let total = 0;
+    for (let root = 1; root <= nodes; root++) {
+      total += uniqTree[root - 1] * uniqTree[nodes - root];
+    }
+    uniqTree[nodes] = total;
+  }
+
+  return uniqTree[n];
+};
