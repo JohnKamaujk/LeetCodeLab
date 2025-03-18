b5909d9b10fd5fa5321d4c5c397b1ead0eddb0df
JohnKamaujk <johnjkkamau@gmail.com>
2025-03-18 13:38:09 +0300

    :white_check_mark: Reverse Linked List

--- linked_list/reverseList.js
+++ linked_list/reverseList.js
@@ -20,7 +20,15 @@ Output: []
 * }
 */
 /**
-* @param {ListNode} head
-* @return {ListNode}
-*/
-var reverseList = function (head) {};
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  if (head == null || head.next == null) return head;
+
+  let newHead = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+
+  return newHead;
+};
