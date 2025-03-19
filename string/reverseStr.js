43db92b9d281b8c4483e641ab5699a79d083d56b
JohnKamaujk <johnjkkamau@gmail.com>
2025-03-19 13:25:38 +0300

    :white_check_mark: Reverse String II

--- string/reverseStr.js
+++ string/reverseStr.js
@@ -20,33 +20,16 @@ var reverseStr = function (s, k) {
   const k2 = 2 * k;
 
   for (let i = 0; i < strArr.length - 1; i += k2) {
-    swap(i, Math.min(i + k - 1, arr.length - 1));
+    swap(i, Math.min(i + k - 1, strArr.length - 1));
   }
 
-  const swap = (end, start) => {
+  function swap(start, end) {
     while (start < end) {
       [strArr[start], strArr[end]] = [strArr[end], strArr[start]];
       start++;
       end--;
     }
-  };
+  }
 
   return strArr.join("");
 };
-
-
-var reverseStr = function (s, k, arr = s.split(""), k2 = k * 2) {
-  for (let i = 0; i < arr.length - 1; i += k2) {
-    swap(i, Math.min(i + k - 1, arr.length - 1));
-  }
-  return arr.join("");
-
-  function swap(start, end) {
-    while (start < end) {
-      [arr[start], arr[end]] = [arr[end], arr[start]];
-      start++;
-      end--;
-    }
-  }
-};
-
