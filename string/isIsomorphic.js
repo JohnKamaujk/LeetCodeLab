c8ecbe7353780f0e809bb5bb012f51895ddcfdc5
JohnKamaujk <johnjkkamau@gmail.com>
2025-03-26 12:54:54 +0300

    :white_check_mark: is isomorphic

--- string/isIsomorphic.js
+++ string/isIsomorphic.js
@@ -30,4 +30,22 @@ Output: true
 * @param {string} t
 * @return {boolean}
 */
-var isIsomorphic = function (s, t) {};
+var isIsomorphic = function (s, t) {
+  if (s.length() !== t.length) return false;
+
+  let mapper = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    if (mapper.get(s[i]) !== undefined) {
+      if (mapper.get(s[i]) !== t[i]) {
+        return false;
+      }
+    } else {
+      mapper.set(s[i], t[i]);
+    }
+  }
+
+  return true;
+};
+
+
