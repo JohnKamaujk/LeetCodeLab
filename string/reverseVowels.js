eae014676002698418be89932543f2f0d53f2421
JohnKamaujk <johnjkkamau@gmail.com>
2025-03-28 16:00:48 +0300

    :white_check_mark: reverse vowels

--- string/reverseVowels.js
+++ string/reverseVowels.js
@@ -17,5 +17,26 @@ Output: "leotcede"
 * @param {string} s
 * @return {string}
 */
-var reverseVowels = function (s) {};
+var reverseVowels = function (s) {
+  const vowels = new Set(["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]);
+  let arr = s.split("");
+  let left = 0,
+    right = arr.length - 1;
 
+  while (left < right) {
+    if (!vowels.has(arr[left])) {
+      left++;
+      continue;
+    }
+
+    if (!vowels.has(arr[right])) {
+      right--;
+      continue;
+    }
+
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+  return arr.join("");
+};
