947066de9d7ce13ea4876833a1027e92ad40e1cd
JohnKamaujk <johnjkkamau@gmail.com>
2025-03-28 13:44:13 +0300

    :white_check_mark: is ugly

--- math/isUgly.js
+++ math/isUgly.js
@@ -22,4 +22,20 @@ Explanation: 14 is not ugly since it includes the prime factor 7.
 * @return {boolean}
 */
 
-var isUgly = function (n) {};
+var isUgly = function (n) {
+  if (n < 1) return false;
+  if (n == 1) return true;
+
+  while (n > 1) {
+    if (n % 2 == 0) {
+      n = n / 2;
+    } else if (n % 3 == 0) {
+      n = n / 3;
+    } else if (n % 5 == 0) {
+      n = n / 5;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
