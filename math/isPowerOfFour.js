e10a3a63d5bbcab393ef9f124d549dfac1675a14
JohnKamaujk <johnjkkamau@gmail.com>
2025-03-28 13:29:57 +0300

    :white_check_mark: is Power of four

--- math/isPowerOfFour.js
+++ math/isPowerOfFour.js
@@ -19,7 +19,7 @@ Output: true
 * @return {boolean}
 */
 var isPowerOfFour = function (n) {
-  if (n <= 0) return false;
   if (n == 1) return true;
+  if (n <= 0 || n % 4 != 0) return false;
   return isPowerOfFour(n / 4);
 };
