5e448a69c8894afc5a6cbf2af331aefa471fba95
JohnKamaujk <johnjkkamau@gmail.com>
2025-04-18 16:58:56 +0300

    :white_check_mark: Intersection Of Two Arrays II

--- array/intersect.js
+++ array/intersect.js
@@ -19,4 +19,19 @@ Constraints:
 * @param {number[]} nums2
 * @return {number[]}
 */
-var intersect = function (nums1, nums2) {}
+var intersect = function (nums1, nums2) {
+  let freqMap = {};
+  let result = [];
+
+  for (const num of nums1) {
+    freqMap[num] = (freqMap[num] || 0) + 1;
+  }
+
+  for(const num of nums2){
+    if(freqMap[num]>0){
+        result.push(num);
+        freqMap[num]--;
+    }
+  }
+    return result;
+};
