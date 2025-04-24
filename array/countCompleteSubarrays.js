ab9ac0b0d3c4e9dc8196af7245baac4eb67d5f20
JohnKamaujk <johnjkkamau@gmail.com>
2025-04-24 12:22:11 +0300

    :white_check_mark: Count complete Sub arrays

--- array/countCompleteSubarrays.js
+++ array/countCompleteSubarrays.js
@@ -5,7 +5,6 @@ We call a subarray of an array complete if the following condition is satisfied:
 
 The number of distinct elements in the subarray is equal to the number of distinct elements in the whole array.
 Return the number of complete subarrays.
-
 A subarray is a contiguous non-empty part of an array.
 
 Example 1:
