db28fbe12895a622e0eec44b87ce0eea4eaf1eed
JohnKamaujk <johnjkkamau@gmail.com>
2025-04-28 16:42:41 +0300

    :white_check_mark: Pascal Triangle

--- array/generate.js
+++ array/generate.js
@@ -1,15 +1,24 @@
 /**
- * @param {number} numRows
- * @return {number[][]}
- */
+Given an integer numRows, return the first numRows of Pascal's triangle.
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+Example 2:
+Input: numRows = 1
+Output: [[1]]
+
+* @param {number} numRows
+* @return {number[][]}
+*/
 var generate = function (numRows) {
   const triangle = [];
 
   for (let row = 0; row < numRows; row++) {
-    const newRow = new Array(row + 1).fill(1); // Start with 1's
-
+    const newRow = new Array(row + 1).fill(1);
     for (let col = 1; col < row; col++) {
-      // Only fill middle elements
       newRow[col] = triangle[row - 1][col - 1] + triangle[row - 1][col];
     }
 
