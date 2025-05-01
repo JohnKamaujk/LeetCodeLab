6581355325cfe2ece99d38c2d448058ebfae1fc9
JohnKamaujk <johnjkkamau@gmail.com>
2025-05-01 15:47:35 +0300

    :white_check_mark: Merge Sorted Arrays

--- array/merge.js
+++ array/merge.js
@@ -37,4 +37,30 @@ nums2.length == n
 * @param {number} n
 * @return {void} Do not return anything, modify nums1 in-place instead.
 */
-var merge = function (nums1, m, nums2, n) {};
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1; // pointer for nums1
+  let j = n - 1; // pointer for nums2
+  let k = m + n - 1; // pointer for the end of nums1
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i -= 1;
+    } else {
+      nums1[k] = nums2[j];
+      j -= 1;
+    }
+    k -= 1;
+  }
+
+  // Copy remaining elements from nums2 (if any)
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    j -= 1;
+    k -= 1;
+  }
+
+  return nums1;
+};
+
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); // Output: [1,2,2,3,5,6]
