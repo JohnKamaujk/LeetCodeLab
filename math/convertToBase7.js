9fca3fc8bbcdbbccbff5fa67289d122d15a08c19
JohnKamaujk <johnjkkamau@gmail.com>
2025-05-12 15:39:39 +0300

    :white_check_mark: Convert to Base 7

--- math/convertToBase7.js
+++ math/convertToBase7.js
@@ -16,4 +16,20 @@ Constraints:
 * @param {number} num
 * @return {string}
 */
-var convertToBase7 = function (num) {};
+var convertToBase7 = function (num) {
+    if (num === 0) return "0";
+    let result = "";
+    let isNegative = num < 0;
+    num = Math.abs(num);
+    while (num > 0) {
+        result = (num % 7) + result;
+        num = Math.floor(num / 7);
+    }
+    return isNegative ? "-" + result : result;
+};
+
+console.log(convertToBase7(100)); // Output: "202"
+console.log(convertToBase7(-7)); // Output: "-10"
+console.log(convertToBase7(0)); // Output: "0"
+console.log(convertToBase7(7)); // Output: "10"
+console.log(convertToBase7(51)); // Output: "102"
