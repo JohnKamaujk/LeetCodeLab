3b14b6ff0ca87b68d91a7444bd4f29b479a43cc9
JohnKamaujk <johnjkkamau@gmail.com>
2025-05-14 16:43:22 +0300

    :white_check_mark: Customer Who Bought All Products

--- database/customer_who_bought_all_products.sql
+++ database/customer_who_bought_all_products.sql
@@ -58,3 +58,8 @@
 -- +-------------+
 -- Explanation: 
 -- The customers who bought all the products (5 and 6) are customers with IDs 1 and 3.
+
+SELECT customer_id 
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key)= (SELECT COUNT(*) FROM Product);
