9140490a93a355c5b3ed25521b1365578c2c5231
JohnKamaujk <johnjkkamau@gmail.com>
2025-05-19 15:56:01 +0300

    :white_check_mark: Type of Triangle

--- math/triangleType.js
+++ math/triangleType.js
@@ -30,8 +30,8 @@ nums.length == 3
 */
 var triangleType = function (nums) {
   if (
-    nums[0] + nums[1] > nums[2] ||
-    nums[1] + nums[2] > nums[0] ||
+    nums[0] + nums[1] > nums[2] &&
+    nums[1] + nums[2] > nums[0] &&
     nums[0] + nums[2] > nums[1]
   ) {
     if (nums[0] === nums[1] && nums[1] === nums[2]) {
