ce5d807e0b22ff093bf308a7b3a8c6694bfb5c08
JohnKamaujk <johnjkkamau@gmail.com>
2025-05-19 15:51:24 +0300

    :white_check_mark: Type of Triangle

--- math/triangleType.js
+++ math/triangleType.js
@@ -28,4 +28,24 @@ nums.length == 3
 * @param {number[]} nums
 * @return {string}
 */
-var triangleType = function (nums) {};
+var triangleType = function (nums) {
+  if (
+    nums[0] + nums[1] > nums[2] ||
+    nums[1] + nums[2] > nums[0] ||
+    nums[0] + nums[2] > nums[1]
+  ) {
+    if (nums[0] === nums[1] && nums[1] === nums[2]) {
+      return "equilateral";
+    } else if (
+      nums[0] === nums[1] ||
+      nums[1] === nums[2] ||
+      nums[0] === nums[2]
+    ) {
+      return "isosceles";
+    } else {
+      return "scalene";
+    }
+  } else {
+    return "none";
+  }
+};
