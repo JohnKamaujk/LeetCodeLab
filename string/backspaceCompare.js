8de1af21cd216a719a861cb0494125bee2dbee71
JohnKamaujk <johnjkkamau@gmail.com>
2025-05-23 15:57:31 +0300

    :white_check_mark: Back Space Compare

--- string/backspaceCompare.js
+++ string/backspaceCompare.js
@@ -35,7 +35,7 @@ var backspaceCompare = function (s, t) {
   }
   for (let i = 0; i < t.length; i++) {
     if (t[i] === "#") {
-      t = s.slice(0, i - 1) + t.slice(i + 1);
+      t = t.slice(0, i - 1) + t.slice(i + 1);
       i--;
     }
   }
