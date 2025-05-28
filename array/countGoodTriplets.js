b6773db2afd6a8048c7b5a4d24af851e67d891b8
JohnKamaujk <johnjkkamau@gmail.com>
2025-05-28 13:22:28 +0300

    :white_check_mark: Count Good Triplets

--- array/countGoodTriplets.js
+++ array/countGoodTriplets.js
@@ -32,4 +32,20 @@ Constraints:
 * @param {number} c
 * @return {number}
 */
-var countGoodTriplets = function (arr, a, b, c) {};
+var countGoodTriplets = function (arr, a, b, c) {
+  let count = 0;
+  for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        if (
+          Math.abs(arr[i] - arr[j]) <= a &&
+          Math.abs(arr[j] - arr[k]) <= b &&
+          Math.abs(arr[i] - arr[k]) <= c
+        ) {
+          count += 1; // Found a good triplet
+        }
+      }
+    }
+  }
+  return count;
+};
