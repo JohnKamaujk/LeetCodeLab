0f34b1e438bda434a2566b6ea68aa2ee20358cac
JohnKamaujk <johnjkkamau@gmail.com>
2025-06-08 14:20:17 +0300

    :white_check_mark: Lexical Order

--- depth_first_search/lexicalOrder.js
+++ depth_first_search/lexicalOrder.js
@@ -17,4 +17,12 @@ Constraints:
 * @param {number} n
 * @return {number[]}
 */
-var lexicalOrder = function (n) {};
+var lexicalOrder = function (n) {
+
+  let result = [];
+  for (let i = 1; i <= n; i++) {
+    result.push(i);
+  }
+  result.sort((a, b) => a.toString().localeCompare(b.toString()));
+  return result;
+};
