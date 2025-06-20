63c50fb9dc50dc28702ee457b93e4e7c0638189f
JohnKamaujk <johnjkkamau@gmail.com>
2025-06-20 17:58:07 +0300

    :white_check_mark: Maximum Difference Between Increasing Elements

--- array/maxDiffIncreasingEl.js
+++ array/maxDiffIncreasingEl.js
@@ -30,4 +30,15 @@ n == nums.length
 * @param {number[]} nums
 * @return {number}
 */
-var maximumDifference = function (nums) {};
+var maximumDifference = function (nums) {
+  let maxDiff = -1;
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] < nums[j]) {
+        maxDiff = Math.max(maxDiff, nums[j] - nums[i]);
+      }
+    }
+  }
+  return maxDiff;
+};
+
