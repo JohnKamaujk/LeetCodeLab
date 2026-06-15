32769f9d7718ffc33b71ee10c38c80c9aa0bf0aa
JohnKamaujk <johnjkkamau@gmail.com>
2026-06-15 11:19:12 +0300

    :white_check_mark: Swap Sex of Employees

--- database/swap_sex_of_employees.sql
+++ database/swap_sex_of_employees.sql
@@ -12,14 +12,11 @@
 -- The sex column is ENUM (category) value of type ('m', 'f').
 -- The table contains information about an employee.
  
-
 -- Write a solution to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
 
 -- Note that you must write a single update statement, do not write any select statement for this problem.
 
 -- The result format is in the following example.
-
- 
 
 -- Example 1:
 
